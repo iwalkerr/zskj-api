0b03ba133231858e63502c85e82ad13fa38f4285
iwalkerr <walkerr@163.com>
2020-12-09 21:10:07 +0800

    add api

--- frontend/core/user/handler/user.go
+++ frontend/core/user/handler/user.go
@@ -2,12 +2,15 @@ package handler
 
 import (
 	"fmt"
-	"xframe/frontend/core/user/logic"
+	userLogic "xframe/frontend/core/user/logic"
+	"xframe/pkg/response"
 
 	"github.com/gin-gonic/gin"
 )
 
 func List(c *gin.Context) {
-	list := logic.New().GetAllUser()
+	list := userLogic.New().GetAllUser()
 	fmt.Println(list)
+
+	response.Success(c).Msg("success").Data(list).JSON()
 }
---
+++ pkg/response/api.go
@@ -0,0 +1,80 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 通用api响应
+type CommonRes struct {
+	Code int         `json:"code"`           // 响应编码 0 成功 500 错误 403 无权限  -1  失败
+	Msg  string      `json:"msg"`            // 消息
+	Data interface{} `json:"data,omitempty"` // 数据内容
+}
+
+// 通用api响应
+type ApiResp struct {
+	r *CommonRes
+	c *gin.Context
+}
+
+//返回一个成功的消息体
+func Success(c *gin.Context) *ApiResp {
+	msg := CommonRes{
+		Code: 0,
+		Msg:  "操作成功",
+	}
+	return &ApiResp{
+		r: &msg,
+		c: c,
+	}
+}
+
+//返回一个错误的消息体
+func Error(c *gin.Context) *ApiResp {
+	msg := CommonRes{
+		Code: 500,
+		Msg:  "操作失败",
+	}
+	return &ApiResp{
+		r: &msg,
+		c: c,
+	}
+}
+
+//返回一个拒绝访问的消息体
+func Forbidden(c *gin.Context) *ApiResp {
+	msg := CommonRes{
+		Code: 403,
+		Msg:  "无操作权限",
+	}
+	return &ApiResp{
+		r: &msg,
+		c: c,
+	}
+}
+
+//设置消息体的内容
+func (resp *ApiResp) Msg(msg string) *ApiResp {
+	resp.r.Msg = msg
+	return resp
+}
+
+//设置消息体的编码
+func (resp *ApiResp) Code(code int) *ApiResp {
+	resp.r.Code = code
+	return resp
+}
+
+//设置消息体的数据
+func (resp *ApiResp) Data(data interface{}) *ApiResp {
+	resp.r.Data = data
+	return resp
+}
+
+//输出json到客户端
+func (resp *ApiResp) JSON() {
+	resp.c.JSON(http.StatusOK, resp.r)
+	resp.c.Abort()
+}
